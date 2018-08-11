b66c1bce3540b7a2466fc0a1a38593acb9738e99
Jonathan Berhe <jonathan.berhe.work@gmail.com>
2018-08-11 18:24:07 +0200

    small changes on main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,13 @@
-// check i f service workers is supported
+// Check if service workers is supported
 if(navigator.serviceWorker)
 {
     console.log("Service Worker: Supported");
+
     window.addEventListener('load', () =>{
         navigator.serviceWorker
-        .register('../service-worker.js')
+        .register('/app/service-worker.js')
         .then(reg => console.log("Service Worker: Registered!"))
-        .catch(err => console.log('Service Worker: error'))
+        .catch(err => console.log('Service Worker: error ' + err.toString()))
     })
 }
 
