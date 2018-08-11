e6ada4dea73dae6317319dfd202c8e7fcb86458e
Jonathan Berhe <jonathan.berhe.work@gmail.com>
2018-08-11 20:51:30 +0200

    add log function with toLocale() API

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,23 @@
+
+function log( msg )
+{
+    var formatDataNow = new Date(Date.now()).toLocaleString('it-IT');
+    console.log(formatDataNow, msg);
+}
+
+
+
 // Check if service workers is supported
 if(navigator.serviceWorker)
 {
-    console.log("Service Worker: Supported");
+    log("Service Worker: Supported");
 
     window.addEventListener('load', () =>{
         navigator.serviceWorker
         .register('../../service-worker.js')
-        .then(reg => console.log("Service Worker: Registered!"))
-        .catch(err => console.log('Service Worker: error ' + err.toString()))
+        .then(reg => log("Service Worker: Registered!"))
+        .catch(err => log('Service Worker: error ' + err.toString()))
     })
 }
+
 
