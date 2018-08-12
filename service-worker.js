08bd625a89c4e3815fa9ff4161da90e56fe66a1b
Jonathan Berhe <jonathan.berhe.work@gmail.com>
2018-08-12 17:19:45 +0200

    mod service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,62 +1,83 @@
 const cache_version = 'v1';
 
 const cache_items = [
-    '/app/js/main.js',
-    '/app/index.html'
+    './app/js/main.js',
+    './app/index.html'
 ];
+
 
 // Call install event
 self.addEventListener('install', event => {
-    console.log(new Date(Date.now()).toLocaleString('it-IT'), "Service Worker: Installed.")
+    log("Service Worker: Installing .")
 
-    event.waitUntil(
-        caches.open(cache_version)
-        .then(function(cache) {
-            console.log(new Date(Date.now()).toLocaleString('it-IT'), "Service Worker: Opened cache..");
-            return cache.addAll(cache_items);
-        })
-    );
+    event.waitUntil( preCache() );
 });
 
-
-// Call activate event
-self.addEventListener('activate', e => {
-    console.log(new Date(Date.now()).toLocaleString('it-IT'), "Service Worker: Activated!")
-
-    e.waitUntil(
-        caches.keys().then( cache_version => {
-            return Promise.all(
-                cache_version.map( cache => {
-                    if( cache !== cache_version)
-                    {
-                        console.log(new Date(Date.now()).toLocaleString('it-IT'), "Service Worker: Clearing old cache...");
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
 
 // Call fetch event
 self.addEventListener('fetch', e => {
-    console.log(new Date(Date.now()).toLocaleString('it-IT'), "Service Worker: Fetching...");
+    e.respondWith( fromCache(e.request) );
+    log("Service Worker: Is serving the assets..");
 
-    e.respondWith(
-        fetch(e.request)
-        .then(res => {
-
-            // Make a copy
-            const resClone = res.clone();
-
-            // Open cache
-            caches.open(cache_version)
-            .then(cache => {
-                cache.put(e.request, resClone);
-            });
-
-            return res;
-
-        }).catch( err => caches.match(e.request).then(res => res))
-    );
+    e.waitUntil( updateCache(e.request) );
+    log(`Service Worker: Cache ${cache_version} si updated.`);
 });
+
+
+
+// f(x) that put items into cache.
+function preCache()
+{
+    return cache.open(cache_version).then(function(cache)
+    {
+        log("Service Worker: Storing assets into cache..");
+
+        // Put arrayItems into cache.
+        return cache.addAll(cache_items);
+    });
+}
+
+
+// f(x) that trying to take items from cache
+function fromCache(request)
+{
+    return caches.open(cache_version).then(function(cache)
+    {
+        log("Service Worker: Taking assets from cache..");
+
+        // Compare actual cache with the request form remote server..
+        return cache.match(request).then(function(matching)
+        {
+            if(matching !== cache_version) log(`Service Worker: Actual cache ${cache_version} !== request; Clearing old cache..`);
+            
+            return matching || caches.delete(cache); 
+        });
+    });
+}
+
+
+function updateCache(request)
+{
+    log(`Service Worker: Opening cache version: ${cache_version} from update event..`)
+    // Take cache_version from caches..
+    return caches.open(cache_version).then(function(cache)
+    {
+        // Fetch request and put the response into cache(cache_version)
+        return fetch(request).then(function(respose)
+        {
+            log("Service Worker: Updating cache form fetch event..")
+            return cache.put(request, respose);
+        })
+        .catch(err => cache.match(request).then(error_return => {
+            log(`Service Worker: ERROR UPDATE CACHE ${cache_version}`);
+            return error_return;
+        }));
+    });
+}
+
+// utility f(x) - console.log
+function log( msg )
+{
+    var formatDataNow = new Date(Date.now()).toLocaleString('it-IT');
+    console.log(formatDataNow, msg);
+}
